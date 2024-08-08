e5ce96ca9cc2347bd71994cbc1d44acdf04c2aac
loteque <andrewandroid@gmail.com>
2024-08-07 18:06:52 -0700

    add asteriod move test to demo, generalize movement

---
+++ lib/ActorInput.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name ActorInput
+
+func get_vector_from_inputs():
+    
+    return Input.get_vector("left", "right", "up", "down")
+
+
+func get_vector_to_left():
+    
+    return Vector2(-1, 0)
--- lib/Movement.gd
+++ lib/Movement.gd
@@ -4,27 +4,27 @@ class_name Movement
 @export var actor: Node2D
 @export var speed: float
 
+@export var input: ActorInput
+
 func has_actor():
-    
-    if actor: return true
+	
+	if actor: return true
 
 
-func get_input():
-    
-    return Input.get_vector("left", "right", "up", "down")
-
-
-
-func update_actor():
-    
-    var input_direction = get_input()
-    actor.velocity = input_direction * speed
-    actor.move_and_slide()
+func update_actor(input_vector: Vector2):
+	
+	actor.velocity = input_vector * speed
+	actor.move_and_slide()
 
 
 func _physics_process(_delta):
-    
-    if !has_actor():
-        return
+	
+	if !has_actor():
+		push_warning("wating for actor")
+		return
 
-    update_actor()
+	if actor.is_in_group("Player"):
+		update_actor(input.get_vector_from_inputs())
+
+	if actor.is_in_group("Obstacle"):
+		update_actor(input.get_vector_to_left())
