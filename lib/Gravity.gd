ff5fb676e65b44d2e0b07f4f8db4adcb0527faf7
loteque <andrewandroid@gmail.com>
2024-08-08 12:14:21 -0700

    implement basic gravitation

--- lib/Gravity.gd
+++ lib/Gravity.gd
@@ -1,4 +1,40 @@
-extends Node
+extends Area2D
 class_name Gravity
 
-@export var grav_area: Area2D
+
+static var ZERO := Vector2.ZERO
+
+var force := Gravity.ZERO
+var grav_direction_vector := Gravity.ZERO
+
+
+static func get_gravity_as_velocity(grav: float, grav_direction: Vector2):
+
+    return grav_direction.normalized() * grav
+
+
+static func get_gravity_direction_vector(area: Area2D, self_area: Area2D):
+    
+    return area.global_position - self_area.global_position
+
+
+func _ready():
+    
+    add_to_group("Grav")
+    area_entered.connect(_on_area_entered)
+    area_exited.connect(_on_area_exited)
+
+
+func _on_area_entered(area: Area2D):
+    
+    if area.is_in_group("Grav"):
+        
+        area.grav_direction_vector = Gravity.get_gravity_direction_vector(area, self)
+        area.force = Gravity.get_gravity_as_velocity(self.gravity, area.grav_direction_vector)
+
+
+func _on_area_exited(area: Area2D):
+    
+    if area.is_in_group("Grav"):
+        
+        area.force = Gravity.ZERO
--- lib/Movement.gd
+++ lib/Movement.gd
@@ -5,26 +5,31 @@ class_name Movement
 @export var speed: float
 
 @export var input: ActorInput
+@export var gravity: Gravity
 
 func has_actor():
 	
-	if actor: return true
+    if actor: return true
 
 
 func update_actor(input_vector: Vector2):
-	
-	actor.velocity = input_vector * speed
-	actor.move_and_slide()
+
+    actor.velocity = input_vector * speed + gravity.force
+    actor.move_and_slide()
 
 
 func _physics_process(_delta):
 	
-	if !has_actor():
-		push_warning("wating for actor")
-		return
+    if !has_actor():
+        
+        push_warning("wating for actor")
+        return
 
-	if actor.is_in_group("Player"):
-		update_actor(input.get_vector_from_inputs())
+    if actor.is_in_group("Player"):
+        
+        update_actor(input.get_vector_from_inputs())
 
-	if actor.is_in_group("Obstacle"):
-		update_actor(input.get_vector_to_left())
+    if actor.is_in_group("Obstacle"):
+        
+        update_actor(input.get_vector_to_left())
+
