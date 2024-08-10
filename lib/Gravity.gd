bcca365e3ca0abc6971e946cfe8e4ec4ebd4831a
loteque <andrewandroid@gmail.com>
2024-08-09 17:00:46 -0700

    gravity now itegrates all forces

--- lib/Gravity.gd
+++ lib/Gravity.gd
@@ -4,24 +4,50 @@ class_name Gravity
 
 static var ZERO := Vector2.ZERO
 
-var force := Gravity.ZERO
-var grav_direction_vector := Gravity.ZERO
-
-
-static func get_gravity_as_velocity(grav: float, grav_direction: Vector2):
-
-    return grav_direction.normalized() * grav
-
 
 static func get_gravity_direction_vector(area: Area2D, self_area: Area2D):
     
-    return area.global_position - self_area.global_position
+    return (area.global_position - self_area.global_position).normalized()
+
+
+static func get_gravity_as_velocity(grav: float, area: Gravity, self_area: Area2D):
+
+    return get_gravity_direction_vector(area, self_area) * grav
+
+
+static func get_inverse_gravity_as_velocity(area, self_area):
+    
+    return get_gravity_as_velocity(self_area.gravity, area, self_area) * -1
+
+
+static func reset_force(area):
+
+    area.force = Gravity.ZERO
+
+
+static func apply_force(area, self_area):
+
+    area.force = area.force + get_gravity_as_velocity(self_area.gravity, area, self_area)
+
+
+static func apply_inverse_force(area, self_area):
+
+    area.force = (area.force + get_inverse_gravity_as_velocity(area, self_area)) * -1
+
+
+static func invert(area):
+
+    area.gravity = area.gravity * -1
+
+
+var force := Gravity.ZERO
+
 
 func _ready():
     
     add_to_group("Grav")
     area_exited.connect(_on_area_exited)
-
+    area_entered.connect(_on_area_entered)
 
 func update():
 
@@ -31,17 +57,29 @@ func update():
 
     for overlap_area in get_overlapping_areas():
         
-        if !overlap_area.is_in_group("Grav"): continue
+        if !overlap_area.is_in_group("Grav"): 
+            
+            continue
         
-        overlap_area.grav_direction_vector = Gravity.get_gravity_direction_vector(overlap_area, self)
-        overlap_area.force = Gravity.get_gravity_as_velocity(self.gravity, overlap_area.grav_direction_vector)
+        Gravity.apply_force(overlap_area, self)
 
 
 func _on_area_exited(area: Area2D):
     
     if area.is_in_group("Grav"):
         
-        area.force = Gravity.ZERO
+        Gravity.reset_force(area)
+
+    if area.is_in_group("BlackHole"):
+
+        Gravity.invert(area)
+
+
+func _on_area_entered(area: Area2D):
+
+    if self.is_in_group("BlackHole"):
+
+        Gravity.invert(area)
 
 
 func _physics_process(_delta):
