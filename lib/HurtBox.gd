53a5249e94f23a2cc1375a8d61c8e6c128c6ffaf
loteque <andrewandroid@gmail.com>
2024-08-10 16:00:58 -0700

    add hitbox and hurtbox

---
+++ lib/HurtBox.gd
@@ -0,0 +1,38 @@
+extends Area2D
+class_name HurtBox
+
+@export var actor: Node2D
+@export var health: int
+
+
+func _ready():
+    area_entered.connect(_on_area_entered)
+
+
+func die():
+
+    if !actor: return
+    
+    actor.queue_free()
+
+
+func damage(area):
+    
+    if area.get_parent() == self.get_parent():
+        
+        return
+
+    if !area.is_in_group("Hitbox"):
+
+        return
+
+    if health <= 0:
+        
+        die(); return
+
+    health = health - area.damage
+
+
+func _on_area_entered(area: Area2D):
+        
+    damage(area)
