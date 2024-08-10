62ab37bed4379e6ff6745bc407eefa37bf6f0577
loteque <andrewandroid@gmail.com>
2024-08-09 20:14:40 -0700

    add asteroid spawner

---
+++ lib/Spawner.gd
@@ -0,0 +1,59 @@
+extends Node2D
+class_name Spawner
+
+@export var timer_name: StringName
+@export var wait_max: float
+@export var spawn_object: PackedScene
+
+
+class SpawnTimer extends Timer:
+
+	var max_range: float
+
+
+	func gen_wait_time():
+		
+		return randf_range(0, max_range)
+
+
+	func _init(timer_name: String, timer_max_range: float):
+		
+		name = timer_name
+		autostart = true
+		max_range = timer_max_range
+		timeout.connect(_on_timeout)
+		wait_time = gen_wait_time()
+
+
+	func _on_timeout():
+		
+		wait_time = gen_wait_time()
+
+
+func _ready():
+	
+	var spawn_timer := SpawnTimer.new(timer_name, wait_max)
+	spawn_timer.timeout.connect(_on_timeout)
+	add_child(spawn_timer)
+
+
+func get_rand_spawn_point():
+	
+	var rand_idx: int = randi_range(0, get_child_count() - 2)
+	return get_child(rand_idx)
+
+
+func _on_timeout():
+
+	var spawn_point: Marker2D = get_rand_spawn_point()
+	var spawn_node: Node2D = spawn_object.instantiate()
+	spawn_point.add_child(spawn_node)
+	
+
+	
+
+
+
+	
+	
+	
