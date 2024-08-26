14e6d1f2b603136b93e1abb490faf60a632aa384
loteque <andrewandroid@gmail.com>
2024-08-26 12:52:10 -0700

    add explosiion sfx

--- player/PlayerHurtBox.gd
+++ player/PlayerHurtBox.gd
@@ -1,14 +1,19 @@
 extends HurtBox
 
+@export var hit_sfx: AudioStreamPlayer
+@export var explode: AudioStreamPlayer
+
 func damage(hitbox: Area2D):
 
     if actor.is_in_group("Player"):
         
+        hit_sfx.play()
         super.damage(hitbox)
         actor_state.health_updated.emit(health)
 
 func die(area: Area2D):
 
+    explode.play()
     actor_state.input_paused = true
     sprite.hide()
 
